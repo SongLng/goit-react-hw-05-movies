6b685aa6251937d0b35f0145374d4c65a80baac9
Sergio Pieshko <sergio.pieshko@icloud.com>
2022-07-04 14:07:25 +0300

    add files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,37 @@
-export const App = () => {
+import { Routes, Route } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+
+import { Navigation } from './Navigation/Navigation';
+import { Header } from './App.styled';
+import { Container } from 'CommonStyled/Common.styled';
+import { HomePage } from './pages/HomePage/HomePage';
+
+import MovieDetailsPage from './pages/MovieDetailsPage/MovieDetailsPage';
+
+const MoviesPage = lazy(() =>
+  import('./pages/MoviePage/MoviePage' /*webpackChunkName: 'MoviePage' */)
+);
+
+const NotFoundPage = lazy(() =>
+  import('./pages/NotFound/NotFoundPage' /*webpackChunkName: 'NotFoundPage' */)
+);
+
+export function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Header>
+        <Navigation />
+      </Header>
+      <Container>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/movies" element={<MoviesPage />} />
+            <Route path="movies/:movieId/*" element={<MovieDetailsPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </Suspense>
+      </Container>
+    </>
   );
-};
+}
---
+++ src/components/Navigation/Navigation.styled.jsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+export const Nav = styled.ul`
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+`;
+
+export const MenuItem = styled.li`
+  margin-left: 30px;
+`;
